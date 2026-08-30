a89cecc0493a69149e09f298781a2ab959899e9a
elreyo-o <cvgenr@gmail.com>
2026-08-30 20:25:45 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Solstice", // required by Discord API, not shown in the client
-        state: "solstice bot",     // this is what people actually see
+        state: "Solstice [+]",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
