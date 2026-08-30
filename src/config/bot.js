f1de60d8d6da8be56253f4e0e1fbec9d6c2dbe35
elreyo-o <cvgenr@gmail.com>
2026-08-30 20:23:32 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,9 +23,9 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "Solstice", // required by Discord API, not shown in the client
         state: "solstice bot",     // this is what people actually see
-        type: 0,               // Custom
+        type: 4,               // Custom
       },
     ],
   },
